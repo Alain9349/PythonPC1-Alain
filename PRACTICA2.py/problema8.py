3bbd98eab6d021b41faee7c4b9b33d8b67a6856a
Alain9349 <h.andrade.nato@gmail.com>
2024-09-28 09:48:57 -0500

    Delete PRACTICA2.py directory

--- PRACTICA2.py/problema8.py
+++
@@ -1,6 +0,0 @@
-print("Ingrese un numero")
-numero=int(input())
-fact=1  
-for i in range(1,numero+1):
-    fact*=i
-print("El factorial de", numero,"!","es:", fact)
--- PRACTICA2.py/problema9.py
+++
@@ -1,5 +0,0 @@
-vocales = {"a", "e", "i", "o", "u", "A", "E", "I", "O","U"}
-palabra = input("Escriba aqui una palabra: ")
-palabra_sin_vocales = "".join(nv for nv in palabra if nv not in vocales)
-print(palabra_sin_vocales)
-
